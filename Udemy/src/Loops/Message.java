61beae8a06034d7be34c730ea68bd293ec6519d7
Rohan Patole <rohanwork1808@gmail.com>
2025-10-14 00:19:26 +0530

    Loops examples

---
+++ Udemy/src/Loops/Message.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class Message {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 9; i++) {
+            System.out.println( i + ": If Java was easy, they would call it Python!");
+        }
+    }
+}
