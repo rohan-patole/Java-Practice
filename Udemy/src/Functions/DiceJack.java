c8115cc2c7a8ed4251b971ecbc1654d86a061fc9
Rohan Patole <rohanwork1808@gmail.com>
2025-10-14 00:23:10 +0530

    Functions

---
+++ Udemy/src/Functions/DiceJack.java
@@ -0,0 +1,18 @@
+package Functions;
+
+public class DiceJack {
+    public static void main(String[] args) {
+        int roll1 = rollDice();
+        int roll2 = rollDice();
+        int roll3 = rollDice();
+
+        System.out.println(roll1);
+        System.out.println(roll2);
+        System.out.println(roll3);
+    }
+    public static int rollDice() {
+        double randomNumber = Math.random() * 6 ;
+        randomNumber +=1;
+        return (int)randomNumber;
+    }
+}
